4032071fddeb85e34cac8acdf55461d425f8f76d
Lily Hopkins <lily@hpkns.uk>
2022-12-30 11:57:52 +0000

    Added missing @GeneratedValue annotation

--- src/main/java/com/softwareinstitute/sakila/models/Actor.java
+++ src/main/java/com/softwareinstitute/sakila/models/Actor.java
@@ -1,11 +1,14 @@
 package com.softwareinstitute.sakila.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Actor {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
